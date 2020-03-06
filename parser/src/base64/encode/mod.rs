e73525e68dd133fffae3074abc45f1a4a85ec318
Zireael <13710677+Zireael-N@users.noreply.github.com>
2020-03-06 16:00:00 +0000

    AVX2 and SSE2 implementations of base64 encoding

--- parser/src/base64/encode/mod.rs
+++ parser/src/base64/encode/mod.rs
@@ -1,5 +1,81 @@
+#[cfg(all(test, target_feature = "avx2"))]
+mod avx2;
 mod scalar;
+#[cfg(all(any(feature = "unsafe", test), target_feature = "ssse3"))]
+mod sse;
 
-pub(crate) fn encode(data: &[u8]) -> String {
-    scalar::encode(data)
+#[inline(always)]
+fn calculate_capacity(data: &[u8]) -> Result<usize, &'static str> {
+    data.len()
+        .checked_mul(4)
+        .and_then(|len| len.checked_add(2))
+        .map(|len| len / 3)
+        .ok_or("cannot calculate capacity without overflowing")
+}
+
+#[cfg(all(feature = "unsafe", target_feature = "ssse3"))]
+pub(crate) fn encode(data: &[u8]) -> Result<String, &'static str> {
+    let mut result = String::with_capacity(calculate_capacity(data)?);
+    unsafe {
+        sse::encode(data, &mut result);
+    }
+    Ok(result)
+}
+
+#[cfg(all(feature = "unsafe", not(target_feature = "ssse3")))]
+pub(crate) fn encode(data: &[u8]) -> Result<String, &'static str> {
+    let mut result = String::with_capacity(calculate_capacity(data)?);
+    unsafe {
+        scalar::encode(data, &mut result);
+    }
+    Ok(result)
+}
+
+#[cfg(not(feature = "unsafe"))]
+pub(crate) fn encode(data: &[u8]) -> Result<String, &'static str> {
+    let mut result = String::with_capacity(calculate_capacity(data)?);
+    scalar::encode(data, &mut result);
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn scalar_and_sse_return_same_values() {
+        let data: Vec<u8> = (0..=255).cycle().take(1024 * 30 + 3).collect();
+
+        let cap = (data.len() * 4 + 2) / 3;
+        let mut buf1 = String::with_capacity(cap);
+        let mut buf2 = String::with_capacity(cap);
+
+        unsafe {
+            scalar::encode(&data, &mut buf1);
+            sse::encode(&data, &mut buf2);
+        }
+
+        assert_eq!(buf1, buf2);
+    }
+
+    #[test]
+    #[cfg(target_feature = "avx2")]
+    fn scalar_and_avx2_return_same_values() {
+        if !is_x86_feature_detected!("avx2") {
+            panic!("AVX2 support is not detected");
+        }
+
+        let data: Vec<u8> = (0..=255).cycle().take(1024 * 30 + 3).collect();
+
+        let cap = (data.len() * 4 + 2) / 3;
+        let mut buf1 = String::with_capacity(cap);
+        let mut buf2 = String::with_capacity(cap);
+
+        unsafe {
+            scalar::encode(&data, &mut buf1);
+            avx2::encode(&data, &mut buf2);
+        }
+
+        assert_eq!(buf1, buf2);
+    }
 }
--- parser/src/base64/encode/scalar.rs
+++ parser/src/base64/encode/scalar.rs
@@ -2,17 +2,18 @@ use crate::base64::byte_map::ENCODE_LUT;
 
 #[cfg(not(feature = "unsafe"))]
 #[inline(always)]
-pub(crate) fn encode(data: &[u8]) -> String {
-    let mut result = Vec::with_capacity((data.len() * 4 + 2) / 3);
+pub(crate) fn encode(data: &[u8], buf: &mut String) {
+    // SAFETY: we'll be pushing ASCII symbols
+    let buf = unsafe { buf.as_mut_vec() };
     let mut chunks = data.chunks_exact(3);
 
     for chunk in chunks.by_ref() {
         let word = u32::from(chunk[0]) + (u32::from(chunk[1]) << 8) + (u32::from(chunk[2]) << 16);
 
-        result.push(ENCODE_LUT[word as u8]);
-        result.push(ENCODE_LUT[(word >> 6) as u8]);
-        result.push(ENCODE_LUT[(word >> 12) as u8]);
-        result.push(ENCODE_LUT[(word >> 18) as u8]);
+        buf.push(ENCODE_LUT[word as u8]);
+        buf.push(ENCODE_LUT[(word >> 6) as u8]);
+        buf.push(ENCODE_LUT[(word >> 12) as u8]);
+        buf.push(ENCODE_LUT[(word >> 18) as u8]);
     }
 
     // This is faster than matching on chunks.remainder().len():
@@ -23,24 +24,21 @@ pub(crate) fn encode(data: &[u8]) -> String {
         word_bitlen += 8;
     }
     while word_bitlen > 0 {
-        result.push(ENCODE_LUT[word as u8]);
+        buf.push(ENCODE_LUT[word as u8]);
         word >>= 6;
         word_bitlen = word_bitlen.saturating_sub(6);
     }
-
-    // SAFETY: ENCODE_LUT contains only ASCII symbols
-    unsafe { String::from_utf8_unchecked(result) }
 }
 
 // About 103% faster
 #[cfg(feature = "unsafe")]
 #[inline(always)]
-pub(crate) fn encode(data: &[u8]) -> String {
-    let mut result: Vec<u8> = Vec::with_capacity((data.len() * 4 + 2) / 3);
+/// SAFETY: the caller must ensure that buf can hold AT LEAST ((s.len() * 4 + 2) / 3) more elements
+pub(crate) unsafe fn encode(data: &[u8], buf: &mut String) {
     let mut chunks = data.chunks_exact(3);
 
-    let mut len = 0;
-    let mut ptr = result.as_mut_ptr();
+    let mut len = buf.len();
+    let mut ptr = buf[len..].as_mut_ptr();
     for chunk in chunks.by_ref() {
         len += 4;
 
@@ -48,17 +46,14 @@ pub(crate) fn encode(data: &[u8]) -> String {
         let b1 = chunk[1];
         let b2 = chunk[2];
 
-        // SAFETY: the Vec should be allocated with sufficient capacity
-        unsafe {
-            ptr.write(ENCODE_LUT[b0]);
-            ptr = ptr.add(1);
-            ptr.write(ENCODE_LUT[((b0 >> 6) | (b1 << 2))]);
-            ptr = ptr.add(1);
-            ptr.write(ENCODE_LUT[((b1 >> 4) | (b2 << 4))]);
-            ptr = ptr.add(1);
-            ptr.write(ENCODE_LUT[(b2 >> 2)]);
-            ptr = ptr.add(1);
-        }
+        ptr.write(ENCODE_LUT[b0]);
+        ptr = ptr.add(1);
+        ptr.write(ENCODE_LUT[((b0 >> 6) | (b1 << 2))]);
+        ptr = ptr.add(1);
+        ptr.write(ENCODE_LUT[((b1 >> 4) | (b2 << 4))]);
+        ptr = ptr.add(1);
+        ptr.write(ENCODE_LUT[(b2 >> 2)]);
+        ptr = ptr.add(1);
     }
 
     let remainder = chunks.remainder();
@@ -68,32 +63,22 @@ pub(crate) fn encode(data: &[u8]) -> String {
             let b0 = remainder[0];
             let b1 = remainder[1];
 
-            // SAFETY: the Vec should be allocated with sufficient capacity
-            unsafe {
-                ptr.write(ENCODE_LUT[b0]);
-                ptr = ptr.add(1);
-                ptr.write(ENCODE_LUT[((b0 >> 6) | (b1 << 2))]);
-                ptr = ptr.add(1);
-                ptr.write(ENCODE_LUT[(b1 >> 4)]);
-            }
+            ptr.write(ENCODE_LUT[b0]);
+            ptr = ptr.add(1);
+            ptr.write(ENCODE_LUT[((b0 >> 6) | (b1 << 2))]);
+            ptr = ptr.add(1);
+            ptr.write(ENCODE_LUT[(b1 >> 4)]);
         }
         1 => {
             len += 2;
             let b0 = remainder[0];
 
-            // SAFETY: the Vec should be allocated with sufficient capacity
-            unsafe {
-                ptr.write(ENCODE_LUT[b0]);
-                ptr = ptr.add(1);
-                ptr.write(ENCODE_LUT[(b0 >> 6)]);
-            }
+            ptr.write(ENCODE_LUT[b0]);
+            ptr = ptr.add(1);
+            ptr.write(ENCODE_LUT[(b0 >> 6)]);
         }
         _ => (),
     }
 
-    // SAFETY: ENCODE_LUT contains only ASCII symbols
-    unsafe {
-        result.set_len(len);
-        String::from_utf8_unchecked(result)
-    }
+    buf.as_mut_vec().set_len(len);
 }
